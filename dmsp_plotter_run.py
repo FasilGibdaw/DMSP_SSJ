8bd2eaec5f5056536f2b80248000180921c3a653
Fasil Tesema <fasgibe@yahoo.com>
2022-10-03 21:41:04 +0300

    updated

--- dmsp_plotter_run.py
+++ dmsp_plotter_run.py
@@ -4,10 +4,11 @@ import numpy as np
 import glob
 files = glob.glob('*dmsp*.cdf')
 files.sort()
-#fname = files[-20]
+print(files[367])
+# fname = files[-20].... 63, 687,
 DD = np.zeros([1, 4])[0]
-for fname in files[0:30]:
-    D = dmsp.dmsp_reader(fname, channel=5)
+for fname in [files[63], files[367], files[687]]:
+    D = dmsp.dmsp_reader(fname, channel=10)
     DD = np.vstack([D, DD])
 x, y, egrided_data, igrided_data = dmsp.dmsp_grid(DD)
 dmsp.dmsp_polar_plot(x, y, egrided_data, igrided_data)
