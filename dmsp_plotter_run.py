83d0c8097dc9e8ef3f6314f5d4cb913e93496f0e
Fasil Tesema <fasgibe@yahoo.com>
2023-05-15 00:59:54 +0300

    updated

--- dmsp_plotter_run.py
+++ dmsp_plotter_run.py
@@ -12,3 +12,6 @@ for fname in [files[0], files[0], files[0]]:
     DD = np.vstack([D, DD])
 x, y, egrided_data, igrided_data = dmsp.dmsp_grid(DD)
 dmsp.dmsp_polar_plot(x, y, egrided_data, igrided_data)
+
+
+##
